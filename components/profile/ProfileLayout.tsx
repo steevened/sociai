3ada0df2a00074adadb6f2f954b557a3730c5cb2
steven <alvaradosteven315@gmail.com>
2023-05-10 17:40:46 -0500

    fix: remove import on deleted component

--- components/profile/ProfileLayout.tsx
+++ components/profile/ProfileLayout.tsx
@@ -1,10 +1,4 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
 import React from 'react';
-import Avatar from '../Avatar';
-import Card from '../atoms/Card';
-import PostCard from '../PostCard';
 import Profile from './Profile';
 
 const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
