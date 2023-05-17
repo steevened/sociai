e7b46152548efe88ca2b75e6352cc30c61bbad50
steven <alvaradosteven315@gmail.com>
2023-05-16 23:43:43 -0500

    prepare enviroment to create post

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -1,9 +1,17 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { NextPageWithLayout } from '../_app';
 import Layout from '@/components/Layout';
+import Cropper from 'react-easy-crop';
 
 const CreatePage: NextPageWithLayout = ({}) => {
-  return <div>index</div>;
+  return (
+    <div className="test">
+      <div className="relative h-96 w-auto ">
+        <div></div>
+        <div className="mx-3 my-2"></div>
+      </div>
+    </div>
+  );
 };
 
 CreatePage.getLayout = (page) => {
