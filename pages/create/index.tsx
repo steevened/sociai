0e701c7cc673dee7a2d1a074c7a1b53be7ea0ab2
steven <alvaradosteven315@gmail.com>
2023-05-16 23:59:27 -0500

    build: create upload picture button input

--- pages/create/index.tsx
+++ pages/create/index.tsx
@@ -1,14 +1,27 @@
-import { FC, useState } from 'react';
 import { NextPageWithLayout } from '../_app';
 import Layout from '@/components/Layout';
 import Cropper from 'react-easy-crop';
+import { CreatePostLogo } from '@/components/icons/Svg';
 
 const CreatePage: NextPageWithLayout = ({}) => {
   return (
-    <div className="test">
-      <div className="relative h-96 w-auto ">
-        <div></div>
-        <div className="mx-3 my-2"></div>
+    <div className="h-full min-h-screen flex">
+      <div className="flex items-center justify-center   w-full flex-col">
+        <CreatePostLogo />
+        <div className="mt-5">
+          <label
+            htmlFor="picture"
+            className="px-4 py-2 bg-sky-700 rounded-md hover:bg-blue-500 duration-200"
+            role="button"
+          >
+            <input
+              type="file"
+              className="appearance-none hidden"
+              id="picture"
+            />
+            <span className="text-md font-medium">Upload Picture</span>
+          </label>
+        </div>
       </div>
     </div>
   );
