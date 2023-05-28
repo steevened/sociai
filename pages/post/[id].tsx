691d3eba073567811c14bfe3ce3e26d94c98c4d8
steven <alvaradosteven315@gmail.com>
2023-05-28 00:11:36 -0500

    build post item page

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -2,21 +2,145 @@ import Layout from '@/components/Layout';
 import { NextPageWithLayout } from '../_app';
 import TopBar from '@/components/atoms/TopBar';
 import { GetServerSideProps } from 'next';
+import { db } from '@/lib/db';
+import { Post, User } from '@/models';
+import { ILikes, Post as PostInterface } from '@/lib/interfaces';
+import Avatar from '@/components/Avatar';
+import MenuDropdown from '@/components/MenuDropdown';
+import Image from 'next/image';
+import Username from '@/components/links/Username';
+import {
+  CommentIcon,
+  LikesIconIn,
+  SaveIconIn,
+  SaveIconOut,
+} from '@/components/icons/Svg';
+import { useEffect, useState } from 'react';
+import { toggleLike, toggleSaved } from '@/lib/services';
+import { usePostById, usePosts, useSaved } from '@/lib/hooks';
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../api/auth/[...nextauth]';
-import { db } from '@/lib/db';
-import { Post } from '@/models';
-import { Post as PostInterface } from '@/lib/interfaces';
 
 interface Props {
-  post: PostInterface;
+  userId: string;
 }
 
-const PostPage: NextPageWithLayout<Props> = ({ post }) => {
+const PostPage: NextPageWithLayout<Props> = ({ userId }) => {
+  const [isLiked, setIsLiked] = useState(false);
+  const [isSaved, setIsSaved] = useState(false);
+
+  const { data: session } = useSession();
+  const { data: saved, mutate: mutateSaved } = useSaved();
+
+  const router = useRouter();
+  const { id } = router.query;
+
+  const {
+    data: post,
+    isLoading,
+    error,
+    mutate: mutatePost,
+  } = usePostById(id as string);
+
+  const { mutate } = usePosts();
   console.log(post);
+
+  const handleLike = async () => {
+    try {
+      const res = await toggleLike(post?._id || '');
+      setIsLiked(res.liked);
+      // console.log(res);
+      // mutate();
+      mutatePost();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSaved = async () => {
+    try {
+      const res = await toggleSaved(post?._id || '');
+      setIsSaved(res.saved);
+      mutateSaved();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    const isLiked = post?.likes.some((like) => (like.user as any) === userId);
+    setIsLiked(isLiked as boolean);
+  }, [post, userId]);
+
+  useEffect(() => {
+    const isSaved = saved?.some((s) => s.post._id === post?._id);
+    setIsSaved(isSaved as boolean);
+  }, [post, saved]);
+
+  if (isLoading || !post) return <div>Loading...</div>;
+
   return (
-    <div>
+    <div className="mb-32">
       <TopBar title={'Post'} />
+      <div className="">
+        <div className="flex items-center justify-between px-4 py-2 ">
+          <div className="flex items-center gap-2">
+            <Avatar imageUrl={post.user.image} />
+            <Username username={post.user.name} id={post.user._id} />
+          </div>
+          <MenuDropdown postId={post._id} />
+        </div>
+        <div className="w-full ">
+          <Image
+            width={2000}
+            height={2000}
+            src={post.image}
+            alt={post.caption || 'image post'}
+          />
+        </div>
+        <div className="px-4 mt-4">
+          <div className="flex">
+            <div className="flex items-center gap-4 grow">
+              <button onClick={handleLike}>
+                <LikesIconIn liked={isLiked} />
+                {/* <LikesIconIn /> */}
+              </button>
+              <button>
+                <CommentIcon />
+              </button>
+            </div>
+            <button onClick={handleSaved}>
+              {isSaved ? <SaveIconOut /> : <SaveIconIn />}
+            </button>
+          </div>
+          <div className="flex flex-col gap-1 mt-4">
+            <div>
+              <button className="text-xs font-semibold">
+                {post.likes.length} like{post.likes.length !== 1 && 's'}
+              </button>
+            </div>
+            <div>
+              <div className="text-sm">
+                <Username
+                  username={post.user.name}
+                  id={post.user._id}
+                  className="mr-1"
+                />
+                <span className="">{post.caption}</span>
+              </div>
+            </div>
+            {post.comments.length > 0 && (
+              <div className="mt-1">
+                <button className="text-sm text-gray-200 text-opacity-50 ">
+                  View all {post.comments.length} comments
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
@@ -26,18 +150,15 @@ export const getServerSideProps: GetServerSideProps = async ({
   res,
   query,
 }) => {
-  const { id } = query;
   const session = await getServerSession(req, res, authOptions);
-  // console.log(session);
-  // if(!session) return
+
   await db.connect();
-  const post = await Post.findOne({ _id: id }).populate('user');
-  console.log(post);
+  const userLogged = await User.findOne({ email: session?.user?.email });
   await db.disconnect();
 
   return {
     props: {
-      post: JSON.parse(JSON.stringify(post)),
+      userId: userLogged ? JSON.parse(JSON.stringify(userLogged?._id)) : '',
     },
   };
 };
