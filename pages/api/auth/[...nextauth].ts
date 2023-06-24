2840435a5295237c886ef231894ff628e282de2a
steven <alvaradosteven315@gmail.com>
2023-06-24 18:36:31 -0500

    edit env variable names

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,8 +8,8 @@ export const authOptions: NextAuthOptions = {
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: process.env.NEXT_PUBLIC_GITHUB_ID || '',
-      clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET || '',
+      clientId: process.env.GITHUB_ID || '',
+      clientSecret: process.env.GITHUB_SECRET || '',
     }),
 
     // ...add more providers here
